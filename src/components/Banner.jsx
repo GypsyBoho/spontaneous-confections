845d37e16dad86368d33a5a37caee8671d400948
Lainey <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-12-01 12:41:56 -0500

    add banner structure

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+//----- IMPORTS -----//
+
+//CSS
+import '../assets/css/Banner.css';
+
+//Images
+// import star from '../assets/images/star.png';
+// import header from '../assets/images/header.png';
+
+//----- EXPORTS -----//
+export default function Banner() {
+
+    //Return
+    return (
+        <section>
+            <p></p>
+            <img src="" alt="" />
+        </section>
+    )
+}
