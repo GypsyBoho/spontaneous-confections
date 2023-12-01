fad4f860f233d5f86a551be7dda1029e73543048
Lainey <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-12-01 12:40:50 -0500

    add footer structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+//----- IMPORTS -----//
+
+//CSS
+import '../assets/css/Footer.css';
+
+//Images
+// import star from '../assets/images/star.png';
+// import header from '../assets/images/header.png';
+
+//----- EXPORTS -----//
+export default function Footer() {
+
+    //Return
+    return (
+        <footer>
+            <div>
+                <h1></h1>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <a href="">
+                    <img src="" alt="" />
+                    <p></p>
+                </a>
+                <a href="">
+                    <img src="" alt="" />
+                    <p></p>
+                </a>
+            </div>
+        </footer>
+    )
+}
