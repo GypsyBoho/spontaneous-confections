d4f4ed9120700a3b82385ee43b0aff7c217a9519
Lainey <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-12-01 11:08:48 -0500

    add file structure and base colors for css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
+//----- IMPORTS -----//
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Pages
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Menu from './pages/Menu.jsx';
+import Contact from './pages/Contact.jsx';
+
+//----- EXPORTS -----//
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+//----- IMPORTS -----//
+
+//CSS
+import '../assets/css/Home.css';
+
+//Images
+// import star from '../assets/images/star.png';
+// import header from '../assets/images/header.png';
+
+//----- EXPORTS -----//
+export default function Home() {
+
+    //Return
+    return (
+        <section>
+            <div>
+                <p></p>
+                <p></p>
+                <button></button>
+            </div>
+        </section>
+    )
+}
