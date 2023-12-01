46fa98f15b4b1f2dbadb641d3410fa708bb926cc
Lainey <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-12-01 11:11:35 -0500

    add about page structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+//----- IMPORTS -----//
+
+//CSS
+import '../assets/css/Home.css';
+
+//Images
+// import star from '../assets/images/star.png';
+// import header from '../assets/images/header.png';
+
+//----- EXPORTS -----//
+export default function Home() {
+
+    //Return
+    return (
+        <section>
+            <div>
+                <h2></h2>
+                <p></p>
+                <p></p>
+            </div>
+            <aside>
+                <img src="" alt="" />
+            </aside>
+        </section>
+    )
+}
