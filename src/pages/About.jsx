414d64d784a5545223ba1542450280215fba90e7
Lainey <dev.lainey@gmail.com>
2023-12-02 10:48:45 -0500

    add about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,7 @@
 import '../assets/css/About.css';
 
 //Images
-// import star from '../assets/images/star.png';
-// import header from '../assets/images/header.png';
+import cookie from '../assets/images/cookie-logo.png';
 
 //----- EXPORTS -----//
 export default function About() {
@@ -14,12 +13,18 @@ export default function About() {
     return (
         <section>
             <div>
-                <h2></h2>
-                <p></p>
-                <p></p>
+                <h2>THE SPARK THAT STARTED IT ALL</h2>
+                <p>Thank you for stopping by! We're passionately
+                    crafting the most exquisite and unique pastries in
+                    Santa Cruz. As a small, veteran-owned baking business,
+                    we deeply appreciate your support for local enterprises.</p>
+                <p>Looking for delicious treats to captivate your guests at
+                    your next event or party? Reach out to us at
+                    baked@spontaneousconfections.com to place your order.
+                    Let us elevate your occasion with our irresistible creations!</p>
             </div>
             <aside>
-                <img src="" alt="" />
+                <img src={cookie} alt="cookie logo" />
             </aside>
         </section>
     )
