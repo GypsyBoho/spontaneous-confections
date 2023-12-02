8554f102f36b69d70b88385718f65f0564b72621
Lainey <laineycreighton@laineys-mbp.mynetworksettings.com>
2023-12-02 10:37:36 -0500

    add footer content

---
+++ README.md
@@ -0,0 +1,8 @@
+Instagram Icon:
+<a target="_blank" href="https://icons8.com/icon/TRVfmt9zm5vL/instagram">Instagram</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+Email Icon:
+<a target="_blank" href="https://icons8.com/icon/2848/letter">Email</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+Phone Icon:
+<a target="_blank" href="https://icons8.com/icon/9730/phone">Phone</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,10 @@
 import '../assets/css/Footer.css';
 
 //Images
-// import star from '../assets/images/star.png';
-// import header from '../assets/images/header.png';
+import cookie from '../assets/images/cookie-logo.png';
+import instagram from '../assets/images/instagram.png';
+import email from '../assets/images/email.png';
+import phone from '../assets/images/phone.png';
 
 //----- EXPORTS -----//
 export default function Footer() {
@@ -14,17 +16,21 @@ export default function Footer() {
     return (
         <footer>
             <div>
-                <h1></h1>
-                <img src="" alt="" />
+                <h1>Spontaneous Confections</h1>
+                <img src={cookie} alt="cookie logo" />
             </div>
             <div>
-                <a href="">
-                    <img src="" alt="" />
-                    <p></p>
+                <a href="https://www.instagram.com/spontaneous_confections/" target='_blank'>
+                    <img src={instagram} alt="instagram" />
+                    <p>@spontaneuos_confections</p>
                 </a>
-                <a href="">
-                    <img src="" alt="" />
-                    <p></p>
+                <a href="mailto: baked@spontaneuosconfections.com">
+                    <img src={email} alt="email" />
+                    <p>baked@spontaneuosconfections.com</p>
+                </a>
+                <a href="tel: 831-480-5166">
+                    <img src={phone} alt="phone" />
+                    <p>(831) 480-5166</p>
                 </a>
             </div>
         </footer>
