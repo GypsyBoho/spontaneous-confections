ea034a00f471d2e6566ff03e7022b8cf67e1432c
BohoBabe <slenorovitz@gmail.com>
2023-12-16 20:01:11 -0800

    updated home page message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,23 +17,23 @@ export default function Home() {
             <div className='box'>
 
                 {/* Bolded Message */}
-                <p className='promo'>See you at the <br /> 11th Annual <br /> Winter Art Market!</p>
+                <p className='promo'>See you at the <br /> Santa Cruz Mountain Brewing!</p>
 
                 {/* Message */}
                 <p className='message'>
                     <a
                         className='address'
-                        href="https://www.google.com/maps/place/Tannery+Arts+Center/@36.9883371,-122.0324681,17z/data=!3m1!4b1!4m6!3m5!1s0x808e4037ac96439d:0x95cefaf6f1f4e06a!8m2!3d36.9883371!4d-122.0298932!16s%2Fg%2F11b88g_0_d?entry=ttu"
+                        href="https://www.eventbrite.com/e/great-brewery-bake-off-holiday-cookie-competition-tickets-773896644697?aff=ebdssbdestsearch"
                         target='_blankl'>
-                        Tannery Arts Center
+                        Santa Cruz Mountain Brewing
                     </a><br />
-                    December 9th <br />
-                    10am - 5pm
+                    December 17th <br />
+                    2pm - 4pm
                 </p>
 
                 {/* Linked Button */}
                 <button>
-                    <a href="https://www.tanneryartscenter.org/winter-art-market-23" target='_blank'>MORE INFO</a>
+                    <a href="https://www.eventbrite.com/e/great-brewery-bake-off-holiday-cookie-competition-tickets-773896644697?aff=ebdssbdestsearch" target='_blank'>MORE INFO</a>
 
                     {/* Link to Contact Form */}
                     {/* <ScrollLink
