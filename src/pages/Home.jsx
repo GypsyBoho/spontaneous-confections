b28ba1e25e4cf76e069cc8b8278c8db0b8037dbe
BohoBabe <slenorovitz@gmail.com>
2024-01-19 11:05:32 -0800

    Updating WHite Box on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,32 +17,33 @@ export default function Home() {
             <div className='box'>
 
                 {/* Bolded Message */}
-                <p className='promo'>Happy 2024 <br /> Let us elevate your occasion with our irresistible creations!</p>
+                <p className='promo'>Happy 2024. <br /> Let us elevate your occasion with our irresistible creations!</p>
 
                 {/* Message */}
-                <p className='message'>
+                {/*<p className='message'>
                     <a
                         className='address'
-                        href="https://www.eventbrite.com/e/great-brewery-bake-off-holiday-cookie-competition-tickets-773896644697?aff=ebdssbdestsearch"
+                        href=""
                         target='_blankl'>
                         Santa Cruz Mountain Brewing
                     </a><br />
                     December 17th <br />
                     1pm - 5pm
-                </p>
+                </p>*/}
 
                 {/* Linked Button */}
                 <button>
-                    <a href="https://www.eventbrite.com/e/great-brewery-bake-off-holiday-cookie-competition-tickets-773896644697?aff=ebdssbdestsearch" target='_blank'>MORE INFO</a>
+                    {/* <a href="" target='_blank'>MORE INFO</a> */}
 
                     {/* Link to Contact Form */}
-                    {/* <ScrollLink
+                    {<ScrollLink
                         to="contact"
                         smooth={true}
                         duration={500}>
                         CONTACT US
-                    </ScrollLink> */}
+                    </ScrollLink>}
                 </button>
+                </section>
             </div>
         </section>
     )
